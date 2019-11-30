2c634004c7c59b1232af97a87f71fc8e647c5173
akmal-ali <akmalali@hotmail.co.uk>
2019-11-30 08:10:01 +0000

    add simple time stub class

---
+++ main/time.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+class Timestamp 
+{
+  public :
+   void getTime();
+   
+};
+
+void
+Timestamp::getTime(){
+  printf("The time is 8:00 am \n");
+}
+
+int main(){
+  Timestamp clock;
+  clock.getTime();
+}
